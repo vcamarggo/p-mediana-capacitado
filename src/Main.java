c4e110243c9753faa980f21c1dfd461b39496f0a
Vinicius <vinicamarggo@hotmail.com>
2017-07-16 17:34:29 -0300

    Versao que pode ser entregue

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,28 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 /**
- * @author Vinícius Camargo
+ * @author Vinícius Camargo , Fernando Carvalho
  *
  * @date 10/07/2017
  * 
- * @opt 17.288,99
+ * @opt 17.288,99 Será?
  */
 class Main {
+    /**
+     * Classe que representa o vertice que sera carregado e mantido inalterado
+     *
+     */
     private static class Vertice {
 	protected int x;
 	protected int y;
 	private short capacidade;
 	private short demanda;
-
-	public int getX() {
-	    return x;
-	}
-
-	public int getY() {
-	    return y;
-	}
 
 	public Vertice(int x, int y, short capacidade, short demanda) {
 	    super();
@@ -33,6 +30,14 @@ class Main {
 	    this.y = y;
 	    this.capacidade = capacidade;
 	    this.demanda = demanda;
+	}
+
+	public int getX() {
+	    return x;
+	}
+
+	public int getY() {
+	    return y;
 	}
 
 	public short getCapacidade() {
@@ -52,12 +57,21 @@ class Main {
 	    return result;
 	}
 
+	/**
+	 * 
+	 * @param medianas
+	 * @return menor mediana que contem capacidade livre
+	 */
 	public Mediana encontraMedianaMaisProximaComCapacidade(List<Mediana> medianas) {
 	    int idxMin = -1;
 	    double currentMin = Double.MAX_VALUE;
 	    double currentVal;
 	    for (int iter = 0; iter < medianas.size(); iter++) {
-		currentVal = medianas.get(iter).findDistance(this);
+		currentVal = medianas.get(iter).encontraDistancia(this); // calcula
+		// distancia
+		// entre o
+		// vertice e
+		// a mediana
 		if (currentVal < currentMin && medianas.get(iter).temCapacidade(demanda)) {
 		    currentMin = currentVal;
 		    idxMin = iter;
@@ -71,21 +85,25 @@ class Main {
 
     }
 
+    /**
+     * 
+     * Classe que e um vertice que representa uma mediana
+     */
     private static class Mediana extends Vertice {
 	private int somaDasDistancias;
 	private short capacidadeUsada;
-
-	public Mediana(int x, int y, short capacidade, short demanda) {
-	    super(x, y, capacidade, demanda);
-	}
 
 	public Mediana(Vertice v) {
 	    super(v.getX(), v.getY(), v.getCapacidade(), v.getDemanda());
 	}
 
-	public double findDistance(Vertice c2) {
-	    int diferencaX = this.getX() - c2.getX();
-	    int diferencaY = this.getY() - c2.getY();
+	/**
+	 * @param vertice
+	 * @return distancia entre a mediana e o vertice
+	 */
+	public double encontraDistancia(Vertice vertice) {
+	    int diferencaX = this.getX() - vertice.getX();
+	    int diferencaY = this.getY() - vertice.getY();
 	    double dist = Math.pow(diferencaX, 2) + Math.pow(diferencaY, 2);
 	    double distance = Math.sqrt(dist);
 	    return distance;
@@ -103,6 +121,16 @@ class Main {
 	    capacidadeUsada += demanda;
 	}
 
+	public boolean temCapacidade(short demanda) {
+	    return capacidadeUsada + demanda < getCapacidade();
+	}
+
+	// Esta funcao assume que o teste de capacidade ja foi realizado
+	public void adicionaDadosVertices(Vertice vertice) {
+	    acrescentaDistancia(encontraDistancia(vertice));
+	    diminuiCapacidade(vertice.getDemanda());
+	}
+
 	@Override
 	public boolean equals(Object obj) {
 	    if (this == obj)
@@ -116,27 +144,28 @@ class Main {
 		return false;
 	    return true;
 	}
-
-	public boolean temCapacidade(short demanda) {
-	    return capacidadeUsada + demanda < getCapacidade();
-	}
-
-	// Esta funcao assume que o teste de capacidade ja foi realizado
-	public void adicionaDadosVertices(Vertice vertice) {
-	    acrescentaDistancia(findDistance(vertice));
-	    diminuiCapacidade(vertice.getDemanda());
-	}
-
     }
 
-    private static class Solucao {
+    /**
+     * Esta classe representa uma instancia da solucao
+     *
+     */
+    private static class Solucao implements Comparable<Solucao> {
 	private double qualidade;
 	private List<Mediana> medianas = new ArrayList<>();
 
-	public Solucao() {
+	public List<Mediana> getMedianas() {
+	    return medianas;
 	}
 
-	public void mutate() {
+	public double getQualidade() {
+	    return qualidade;
+	}
+
+	/**
+	 * operador de mutacao
+	 */
+	public void fazMutacao() {
 	    Random rand = new Random();
 	    int index = rand.nextInt(nrMedianas);
 	    this.getMedianas().remove(index);
@@ -152,7 +181,7 @@ class Main {
 	    } while (!trocou);
 	}
 
-	public void evaluate(int indicesPercorridos) {
+	public void avaliarQualidadeSolucao(int indicesPercorridos) {
 	    if (nrVertices - indicesPercorridos > 0) {
 		penaliza(PENALIZACAO + (1 - (indicesPercorridos / nrVertices)));
 	    }
@@ -161,28 +190,18 @@ class Main {
 		fitness += mediana.getSomaDasDistancias();
 	    }
 	    qualidade = fitness;
-	    System.out.println(" Qualidade LOCAL = " + qualidade);
 	}
 
-	public List<Mediana> getMedianas() {
-	    return medianas;
-	}
-
-	public double getQualidade() {
-	    return qualidade;
-	}
-
-	public int geraRand() {
+	public int geraSolucaoAleatoria() {
 	    criaPrimeirasMedianas();
-	    return ligaVerticesAsMedianasERetornaUltimoIndice();
+	    return ligaVerticesAsMedianasERetornaUltimoIndiceUsado();
 	}
 
-	private int ligaVerticesAsMedianasERetornaUltimoIndice() {
+	private int ligaVerticesAsMedianasERetornaUltimoIndiceUsado() {
 	    int nrIndicesAdicionados = 0;
 
 	    for (int j = 0; j < nrVertices; j++) {
 		if (!(vertices[j] instanceof Mediana)) {
-		    // TODO testar se esta mediana será alterada
 		    Mediana mediana = vertices[j].encontraMedianaMaisProximaComCapacidade(getMedianas());
 		    if (mediana != null) {
 			mediana.adicionaDadosVertices(vertices[j]);
@@ -214,34 +233,52 @@ class Main {
 	    qualidade = qualidade * porcentagemPenalizacao;
 	}
 
+	@Override
+	public int compareTo(Solucao o) {
+	    return (qualidade > o.getQualidade()) ? 1 : (qualidade < o.getQualidade()) ? -1 : 0;
+	}
+
     }
 
+    /**
+     * Esta classe representa o conjunto de todas solucoes
+     *
+     */
     private static class Populacao {
-	private static Solucao[] solucoes;
+	private Solucao[] solucoes;
 	private double totalFitness;
 
 	public Populacao() {
 	    solucoes = new Solucao[TAM_POP];
 
-	    // init population
 	    for (int i = 0; i < TAM_POP; i++) {
 		solucoes[i] = new Solucao();
-
-		int indicesPercorridos = solucoes[i].geraRand();
-		solucoes[i].evaluate(indicesPercorridos);
+		int indicesPercorridos = solucoes[i].geraSolucaoAleatoria();
+		solucoes[i].avaliarQualidadeSolucao(indicesPercorridos);
 	    }
-	    this.evaluateTotal();
+
+	    this.avaliarQualidadePopulacao();
 	}
 
-	public Solucao[] crossover(Solucao velha0, Solucao velha1) {
+	public void setPopulation(Solucao[] newPop) {
+	    System.arraycopy(newPop, 0, solucoes, 0, TAM_POP);
+	}
+
+	public Solucao[] getSolucoes() {
+	    return solucoes;
+	}
+
+	/**
+	 * Operador de reprodumecao
+	 * 
+	 * @param velha0
+	 * @param velha1
+	 * @return duas solucoes baseadas nos parametros
+	 */
+	public Solucao[] fazCruzamento(Solucao velha0, Solucao velha1) {
 	    Solucao[] newIndiv = new Solucao[2];
 	    newIndiv[0] = new Solucao();
 	    newIndiv[1] = new Solucao();
-
-	    if (velha0.getMedianas().size() != 10)
-		System.out.println("merda");
-	    if (velha1.getMedianas().size() != 10)
-		System.out.println("merda");
 
 	    int randPoint = randomizer.nextInt(nrMedianas);
 	    int i;
@@ -257,16 +294,17 @@ class Main {
 		}
 	    }
 
-	    int indicesPercorridos0 = newIndiv[0].ligaVerticesAsMedianasERetornaUltimoIndice();
-	    newIndiv[0].evaluate(indicesPercorridos0);
+	    int indicesPercorridos0 = newIndiv[0].ligaVerticesAsMedianasERetornaUltimoIndiceUsado();
+	    newIndiv[0].avaliarQualidadeSolucao(indicesPercorridos0);
 
-	    int indicesPercorridos1 = newIndiv[1].ligaVerticesAsMedianasERetornaUltimoIndice();
-	    newIndiv[1].evaluate(indicesPercorridos1);
+	    int indicesPercorridos1 = newIndiv[1].ligaVerticesAsMedianasERetornaUltimoIndiceUsado();
+	    newIndiv[1].avaliarQualidadeSolucao(indicesPercorridos1);
 
 	    return newIndiv;
 	}
 
-	public Solucao findBestSolucao() {
+	// A melhor solucao e a que tem menor qualidade (soma das distancia)
+	public Solucao encontraMelhorSolucaoDaLista() {
 	    int idxMin = 0;
 	    double currentMin = 1.0;
 	    double currentVal;
@@ -278,11 +316,10 @@ class Main {
 		    idxMin = idx;
 		}
 	    }
-
-	    return solucoes[idxMin]; // minimization
+	    return solucoes[idxMin];
 	}
 
-	public double evaluateTotal() {
+	public double avaliarQualidadePopulacao() {
 	    this.totalFitness = 0.0;
 	    for (int i = 0; i < TAM_POP; i++) {
 		this.totalFitness += solucoes[i].getQualidade();
@@ -290,31 +327,26 @@ class Main {
 	    return this.totalFitness;
 	}
 
-	public Solucao rouletteWheelSelection() {
-	    // double randNum = randomizer.nextDouble() * this.totalFitness;
+	/**
+	 * Operador de selecao
+	 */
+	public Solucao selecaoDeRoleta() {
+	    double randNum = randomizer.nextDouble() * (this.totalFitness / 2);
 	    int idx = randomizer.nextInt(TAM_POP);
-	    // for (idx = 0; idx < TAM_POP && randNum > 0; ++idx) {
-	    // randNum -= solucoes[idx].getQualidade();
-	    // }
+	    for (idx = 0; idx < TAM_POP && randNum > 0; ++idx) {
+		randNum -= solucoes[idx].getQualidade();
+	    }
 	    return solucoes[idx];
 	}
 
-	public double getTotalFitness() {
-	    return totalFitness;
-	}
-
-	public void setPopulation(Solucao[] newPop) {
-	    // this.m_population = newPop;
-	    System.arraycopy(newPop, 0, solucoes, 0, TAM_POP);
-	}
     }
 
-    private static final short TEMPO_MAX_MILISSEGUNDOS = 8000;
-    private static final short ELITISMO = 0;
-    private static final float TAXA_MUTACAO = 0.0f;
-    private static final short TAM_POP = 20;
-    private static final short MAX_ITER = 20;
-    private static final short PENALIZACAO = 10;
+    private static final short TEMPO_MAX_MILISSEGUNDOS = 10000;
+    private static final short ELITISMO = 100;
+    private static final float TAXA_MUTACAO = 0.05f;
+    // private static final short ITER_SEM_MOD = 50;
+    private static final short TAM_POP = 5000;
+    private static final short PENALIZACAO = 20000;
 
     private static int nrVertices;
     private static int nrMedianas;
@@ -331,59 +363,61 @@ class Main {
 	}
 	scan.close();
 
-	pMedianaCapacitada();
+	System.out.println(pMedianaCapacitada());
     }
 
-    private static void pMedianaCapacitada() {
+    private static double pMedianaCapacitada() {
 	Populacao pop = new Populacao();
 	Solucao[] newPop = new Solucao[TAM_POP];
 	Solucao[] indiv = new Solucao[2];
 
 	long tempoFinal = System.currentTimeMillis() + TEMPO_MAX_MILISSEGUNDOS;
 	int count;
-	// for (int iter = 0; iter < MAX_ITER && tempoFinal >
-	// System.currentTimeMillis(); iter++) {
-	for (int iter = 0; iter < MAX_ITER; iter++) {
-	    count = 0;
 
-	    // Elitism
+	// double melhor = Double.MAX_VALUE;
+	// for (int nrSemMod = 0; nrSemMod < ITER_SEM_MOD;) {
+	for (; tempoFinal > System.currentTimeMillis();) {
+	    count = 0;
+	    Arrays.sort(pop.getSolucoes());
+
+	    /*
+	     * if (melhor <= pop.getSolucoes()[0].getQualidade()) { nrSemMod++;
+	     * } else { melhor = pop.getSolucoes()[0].getQualidade(); nrSemMod =
+	     * 0; System.out.println(melhor); }
+	     */
+
+	    // Conceito de Elitismo
 	    for (int x = 0; x < ELITISMO; ++x) {
-		// CORRIGIR
-		newPop[count] = pop.findBestSolucao();
+		newPop[count] = pop.encontraMelhorSolucaoDaLista();
 		count++;
 	    }
 	    while (count < TAM_POP) {
-		Solucao pai = pop.rouletteWheelSelection();
-		Solucao mae = pop.rouletteWheelSelection();
-		indiv = pop.crossover(mae, pai);
+		Solucao pai = pop.selecaoDeRoleta();
+		Solucao mae = pop.selecaoDeRoleta();
+		indiv = pop.fazCruzamento(mae, pai);
 
-		// Mutation
+		 // Conceito de Mutacao
 		if (randomizer.nextDouble() < TAXA_MUTACAO) {
-		    indiv[0].mutate();
-		    int indicesPercorridos = indiv[0].ligaVerticesAsMedianasERetornaUltimoIndice();
-		    indiv[0].evaluate(indicesPercorridos);
+		    indiv[0].fazMutacao();
+		    int indicesPercorridos = indiv[0].ligaVerticesAsMedianasERetornaUltimoIndiceUsado();
+		    indiv[0].avaliarQualidadeSolucao(indicesPercorridos);
 		}
 		if (randomizer.nextDouble() < TAXA_MUTACAO) {
-		    indiv[1].mutate();
-		    int indicesPercorridos = indiv[1].ligaVerticesAsMedianasERetornaUltimoIndice();
-		    indiv[1].evaluate(indicesPercorridos);
+		    indiv[1].fazMutacao();
+		    int indicesPercorridos = indiv[1].ligaVerticesAsMedianasERetornaUltimoIndiceUsado();
+		    indiv[1].avaliarQualidadeSolucao(indicesPercorridos);
 		}
 
-		// add to new population
 		newPop[count] = indiv[0];
 		newPop[count + 1] = indiv[1];
 		count += 2;
 	    }
+	    // Conceito de Nova Geracao Completa
 	    pop.setPopulation(newPop);
-
-	    // reevaluate current population
-	    pop.evaluateTotal();
-	    System.out.print("Qualidade total = " + pop.getTotalFitness());
-	    System.out.println(" Máxima qualidade = " + pop.findBestSolucao().getQualidade());
+	    pop.avaliarQualidadePopulacao();
 	}
 
-	Solucao bestIndiv = pop.findBestSolucao(); // {//
-	System.out.println(bestIndiv.getQualidade());
+	return pop.encontraMelhorSolucaoDaLista().getQualidade();
     }
 
 }
